4e53a62e609eca3833a63628055d07db6142f012
bobi <b.melnychuk@dev-branch.com>
2023-04-11 08:07:48 +0300

    Lesson 4 Data type

---
+++ lesson_4/js/script.js
@@ -0,0 +1,21 @@
+// #1
+let userAge = 36;
+let userInfo = `freelancer ${userAge}`;
+
+console.log(userInfo)
+
+//#2
+
+let userHeight = 145 / 0;
+
+console.log(userHeight)
+
+//#3
+let userName;
+
+console.log(userName)
+
+//#
+let userSize = "45" / "8";
+
+console.log(typeof userSize)
