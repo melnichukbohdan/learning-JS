de27fa47f2a679dc64162d7aa581703a726f7f97
bobi <b.melnychuk@dev-branch.com>
2023-04-12 07:13:21 +0300

    Lesson 5 Operators

---
+++ lesson_5/js/script.js
@@ -0,0 +1,21 @@
+//#1
+
+console.log('35' + - "22");
+
+//#2
+console.log('35' * "22");
+
+//#3
+//console.log('558' > 22++);
+
+//#4
+let userCounter = 0;
+let newUser = userCounter++;
+console.log(newUser);
+
+//#5
+console.log(!false && 11 || 18 && !'');
+
+//#6
+let name = 0;
+console.log(name ?? "no name");
