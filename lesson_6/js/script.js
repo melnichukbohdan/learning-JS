11451ce9b13be366802c97c3774d0c501963f9a9
bobi <b.melnychuk@dev-branch.com>
2023-04-12 22:02:37 +0300

    Lesson 6 if

---
+++ lesson_6/js/script.js
@@ -0,0 +1,25 @@
+// #1
+if ( 1 === "1") {
+    console.log('true');
+} else {
+    console.log('false')
+}
+
+// #2
+if ( 5 == "5") {
+    console.log('true');
+} else {
+    console.log('false')
+}
+
+// #3
+
+let message = (92 > '11' && 58 < 100) ? 'true' : 'false';
+console.log(message)
+
+// #4
+if ( 0 ) {
+    console.log('true');
+} else {
+    console.log('false')
+}
