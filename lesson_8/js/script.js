0455a833c7af0c9613df01051e536b7f41147ffe
bobi <b.melnychuk@dev-branch.com>
2023-04-15 22:01:06 +0300

    Lesson 8 function

--- lesson_8/js/script.js
+++ lesson_8/js/script.js
@@ -32,4 +32,59 @@ if (2 > 1) {
     };
 }
 
-getSum();
+getSum()
+
+// // function + cycle
+// function calcSum(numOne, numTwo) {
+//     let result = 1;
+//     for (let i = 0; i < numTwo; i++) {
+//         result *= numOne;
+//     }
+//
+//     return result;
+// }
+//
+// console.log(calcSum(2, 3));
+
+// recursive function
+
+function calcSum(numOne, numTwo) {
+    if (numTwo === 1) {
+        return numOne;
+    } else {
+        return numOne * calcSum(numOne, numTwo -1);
+    }
+}
+
+console.log(calcSum(2 , 3));
+
+// arrow function
+
+let getMessage = (text, name) => {
+    let message = text + ', ' + name + '!';
+    return message;
+}
+console.log(getMessage('Hello', 'World'));
+
+//setTimeout
+function showHello(text, name) {
+    console.log(`${text}, ${name}!`);
+}
+
+setTimeout(showHello, 3000, 'Hello', 'Vasia');
+
+//setInterval
+setInterval(showHello, 500, 'Hello', 'Petia');
+
+//clearInterval()
+
+let result = 0;
+function showNumber(num) {
+    result += num;
+    console.log(result);
+    if (result === 5) {
+        clearInterval(timeId);
+    }
+}
+
+let timeId = setInterval(showNumber, 1000, 1);
